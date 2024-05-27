4d44e0104b6acf356f0e5868c2f0b554162f62a1
Jan-Andreas Rusnak <42783523+redmawzx@users.noreply.github.com>
2024-05-27 02:37:21 +0200

    add file init.lua

---
+++ lua/redmaw/core/init.lua
@@ -0,0 +1,1 @@
+require('redmaw.core.options')
--- lua/redmaw/core/options.lua
+++ lua/redmaw/core/options.lua
@@ -6,3 +6,35 @@ local opt = vim.opt
 -- Show line numbers
 opt.number = true
 opt.relativenumber = true
+
+-- Use 2 spaces for Tab indenting
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- Disable line wrapping
+opt.wrap = false
+
+-- Search settings to only case sensitive if mixed case
+opt.smartcase = true
+opt.ignorecase = true
+
+-- Horizontal highlight on cursor
+opt.cursorline = true
+
+-- Color settings
+opt.termguicolors = true
+opt.background = 'dark'
+opt.signcolumn = 'yes'
+
+-- Backspace on indent/end of line/insert mode start position
+opt.backspace = 'indent,eol,start'
+
+-- Enable splitting windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- Disable swap file
+opt.swapfile = false
