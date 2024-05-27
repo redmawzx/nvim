bcb41f091d3225fdcc5fd07173d3f49deac78990
Jan-Andreas Rusnak <42783523+redmawzx@users.noreply.github.com>
2024-05-27 20:47:59 +0200

    remove copilot, add lsp support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
