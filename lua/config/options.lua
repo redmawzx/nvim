18ac11619929fea07263a47955ee1ae6ce07568f
Jan-Andreas Rusnak <42783523+redmawzx@users.noreply.github.com>
2024-05-27 19:17:41 +0200

    vim changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,26 @@
--- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.scriptencoding = "utf-8"
+
+local opt = vim.opt
+
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+-- Current buffername as title
+opt.title = true
+
+-- Autoindent
+opt.autoindent = true
+
+-- Command previews
+opt.inccommand = "split"
+
+-- Indent if text does not fit
+opt.breakindent = true
+
+-- Allow backspace over indentation, line breaks and start of insert
+opt.backspace = { "start", "eol", "indent" }
+
+opt.path:append({ "**" })
+-- Ignore node modules
+opt.wildignore:append({ "*/node_modules/*" })
---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,6 @@
+return {
+  "goolord/alpha-nvim",
+  opts = function(_, opts)
+    local logo = [[]]
+  end,
+}
