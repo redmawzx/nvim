3753aaaec4ae0448a042b6a0ade3230344c4223a
Jan-Andreas Rusnak <42783523+redmawzx@users.noreply.github.com>
2024-05-27 03:12:11 +0200

    more keymaps

--- lua/redmaw/core/keymaps.lua
+++ lua/redmaw/core/keymaps.lua
@@ -4,14 +4,30 @@ vim.g.maplocalleader = ' '
 
 local keymap = vim.keymap
 
+-- Enter normal mode with 'jk'
+keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode' })
+
 -- Disable arrow keys for moving
 keymap.set('n', '<left>', '<cmd>echo "Use h to move!"<CR>')
 keymap.set('n', '<right>', '<cmd>echo "Use l to move!"<CR>')
 keymap.set('n', '<up>', '<cmd>echo "Use k to move!"<CR>')
 keymap.set('n', '<down>', '<cmd>echo "Use j to move!"<CR>')
 
+-- Create split windows
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' })
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' })
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
+keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
+
 -- Navigate between split windows
 keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move to the left window' })
 keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move to the right window' })
 keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move to the lower window' })
 keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move to the upper window' })
+
+-- Remove search highlights
+keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
+
+-- Increment/Decrement numbers
+keymap.set('n', '<leader>+', '<C-a>', { desc = 'Increment number' })
+keymap.set('n', '<leader>-', '<C-x>', { desc = 'Decrement number' })
