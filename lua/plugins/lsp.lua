c6a2466cf94235c7dc6a840e0b6acd6269f87243
redmawzx <42783523+redmawzx@users.noreply.github.com>
2024-11-14 16:10:33 +0100

    autopairs
    added new autopairs plugin and rename

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "luacheck",
+        "prettier",
         "shellcheck",
         "shfmt",
         "tailwindcss-language-server",
